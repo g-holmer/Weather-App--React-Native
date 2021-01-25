ac8a8826227818c49baacec86774fccb00d2bb4a
g-holmer <gabbeholmer@hotmail.com>
2021-01-25 13:00:13 +0100

    add

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { API_KEY } from "./Utils/ApiKey";
+import Weather from "./Components/Weather";
+import axios from "axios";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [temperature, setTemperature] = useState(0);
+  const [weatherCondition, setWeatherCondition] = useState(null);
+  const [error, setError] = useState(null);
+  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        setError("Error getting position.");
+      }
+    );
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+      )
+      .then(function (json) {
+        // handle success
+        console.log(json.data);
+        setTemperature(json.data.main.temp);
+        setWeatherCondition(json.data.weather[0].main);
+        setIsLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <Weather weather={weatherCondition} temperature={temperature} />
+      )}
     </View>
   );
 }
@@ -14,8 +54,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Utils/ApiKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = "ba50a4dde0dd7ff6de662468ff6855e1";
